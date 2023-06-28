f9e29a9096e53d8b76c428ab156e0a97d38b4915
Deepesh Agrawal <99245631+Deepesh76780@users.noreply.github.com>
2023-06-28 13:37:28 +0530

    Update app.js

--- app.js
+++ app.js
@@ -64,7 +64,7 @@ app.use(errorController.get404);
 mongoose
   .connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then((result) => {
-    app.listen(process.env.PORT || 3001, () => {
+    app.listen(process.env.PORT, () => {
       console.log("running app");
     });
   })
